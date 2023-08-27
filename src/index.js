6c6cbb1b0d60ba46bc34a24020c3b3aa1bccb87b
Softphoenix <gemjiffy@gmail.com>
2023-08-27 15:30:11 +0100

    cont of work

--- src/index.js
+++ src/index.js
@@ -1,32 +1,49 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import  creatRoot  from "react-dom";
+import './index.css';
+
+const firstBook = {
+  img:"https://m.media-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  title: 'I Love you to the moon and Back',
+  author: 'Amelia hepworth',
+};
+const secondBook = {
+  img:"https://m.media-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  title: 'Our class is a family',
+  author: 'Shannon Olsen ',
+};
+
 
 function BookList(){
   return (
-   <section>
-    <Book />
+   <section className="booklist">
+    <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}> 
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, dignissimos. Quos dolorum a dolores ut blanditiis labore eum quae ea.
+      </p>
+    
+    </Book>
+    <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
 
    </section>
   );
 
 }
-
-const Book = () => {
+  
+// const Book = ({ img, title, author, children }) => {
+  const Book = (props) => {
+  const { img, title, author,} = props;
+  console.log(props)
   return (
-    <article>
-      <Image/>
-      <Tittle/>
-      <Author/>
+    <article className="book">
+       <img src={img} alt="" /> 
+         <h1>{title}</h1>
+         <h4>{author}</h4>
+         {props.children} 
+
     </article>
-  )
+  );
 }
 
-const Image  = () => (
-  <img src="" alt="" />
-)
-
-const Tittle = () => <h1>I Love you to the moon and Back</h1>
-const Author = () => <h4>Amelia hepworth</h4>
-
-ReactDOM.render(<BookList/>, document.getElementById('root')
+creatRoot.render(<BookList/>, document.getElementById('root')
 );
